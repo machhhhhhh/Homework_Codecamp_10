bc9e91e5af75d3b30282f08edf9d0c557beb0a13
machhhhhhh <maygo0008@gmail.com>
2022-06-22 17:43:41 +0700

    re css for default

--- Project492/client/src/page/Register.js
+++ Project492/client/src/page/Register.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../components/css/register.css'
+import {useNavigate} from 'react-router-dom'
 
 function Register() {
+
+  const navigate = useNavigate()
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirm, setConfirm] = useState('')
+  const [firstname, setFirstname] = useState('')
+  const [lastname, setLastname] = useState('')
+  const [phone, setPhone] = useState('')
+
   return (
-    <div>Register</div>
+    <div className='register'>
+
+
+    </div>
   )
 }
 
--- Project492/client/src/privateRoute/PrivateRoute.js
+++ Project492/client/src/privateRoute/PrivateRoute.js
@@ -11,7 +11,7 @@ import axios from 'axios'
 function PrivateRoute(props) {
 
     const role = props.role || 'guest' 
-    const [user, setUser] = useState(null)
+    // const [user, setUser] = useState(null)
 
     const navigate = useNavigate()
 
@@ -19,17 +19,22 @@ function PrivateRoute(props) {
         if(role === 'guest') navigate('/')
         if(role === 'customer') navigate('/index')
         if(role === 'shop') navigate('/home')
+        
 
         console.log(role);
 
-        // const fetchUser = async () => {
-        //     const result = await axios.get('/user')
-        //     console.log(result);
-        // }
+        const fetchUser = async () => {
+            
+            const result = await axios.get('/user')
+            console.log(result);
 
-        // if(role!=='guest') fetchUser()
+        }
+
+        // if(role!=='guest') 
+        // fetchUser()
 
     },[role])
+
 
     const logout = async() => {
         props.setRole('guest')
@@ -45,7 +50,6 @@ function PrivateRoute(props) {
 
         {role==='guest' && (
             <>
-                <Route path='*' element={<Login role ={role} setRole={props.setRole}  /> }  exact />
                 <Route path='/' element={<Login setRole={props.setRole}  role= {role} /> }  exact />
                 <Route path='/register' element={<Register/>}  exact/>
                 
@@ -54,16 +58,14 @@ function PrivateRoute(props) {
 
         {role ==='customer' && (
             <>
-                <Route path='*' element={<Index role = {role} logout={logout} user = {user} />} exact/>
-                <Route path='/index' element={<Index  role = {role} logout={logout} user = {user} />} exact/>
+                <Route path='/index' element={<Index  role = {role} logout={logout}  />} exact/>
             </>
         )}
 
 
         {role ==='shop' && (
             <>
-                <Route path='*' element={<Home logout={logout} user = {user} />} exact />
-                <Route path='/home' element={<Home logout={logout}  user = {user}/>} exact />
+                <Route path='/home' element={<Home logout={logout}  />} exact />
             </>
         )}
 
